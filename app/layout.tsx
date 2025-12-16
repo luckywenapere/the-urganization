e3e1e134b91841454c248c8ed7516c4afcddb8e6
unknown <luckydio10@gmail.com>
2025-12-16 09:16:46 +0100

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,13 +29,14 @@ export default function RootLayout({
     <html lang="en" className={`${spaceGrotesk.variable} ${dmSans.variable}`}>
       <head>
         {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-X4PX21EV8K"></script>
         <script dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-XXXXXXXXXX');
+
+            gtag('config', 'G-X4PX21EV8K');
           `,
         }} />
       </head>
