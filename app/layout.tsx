93b4be2f9dd12a2ab0547c05f5292ffb5b3d0ea7
unknown <luckydio10@gmail.com>
2025-12-17 12:51:04 +0100

    added more meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,50 @@ const dmSans = DM_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Urganize",
-  description: "A workspace for artist teams to turn ideas into real music and projects. Plan releases, stay organized, and focus on creating your best work.",
+  title: {
+    default: 'Urganize | Focus. Finish. Release.',
+    template: '%s - Urganize',
+  },
+  description:
+    'A workspace for artist teams to turn ideas into real music and projects. Plan releases, stay organized, and focus on creating your best work.',
+  verification: {
+    google: '',
+  },
+
+  metadataBase: new URL('https://urganize.app'),
+  openGraph: {
+    title: 'Focus on making the best music with Urganize',
+    description:
+      'A workspace for artist teams to turn ideas into real music and projects. Plan releases, stay organized, and focus on creating your best work.',
+    url: 'https://urganize.app',
+    siteName: 'Urganize',
+    locale: 'en_US',
+    type: 'website',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Urganize - Music Project Management Application',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: ['/opengraph-image.png'],
+  },
+  keywords: [
+    'Music Project Management',
+    'Artist Collaboration',
+    'Release Planning',
+    'Music Team Organization',
+    'Creative Workflow',
+    'Music Production Management',
+    'Artist Team Workspace',
+    'Music Project Tracking',
+    'Collaboration for Musicians',
+    'Music Industry Tools',
+  ],
 };
 
 export default function RootLayout({
